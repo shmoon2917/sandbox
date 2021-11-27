c4faa0f270f51470ec566435fa6dd786fa8128d7
Sangho Moon <shmoon2917@jirocorp.io>
2021-11-27 18:28:01 +0900

    chore: eslint, prettier 패키지 추가 및 환경 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const App = () => {
+const App: React.FC = (): JSX.Element => {
   return <div>hello world!</div>;
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,83 @@
+const path = require('path');
+const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const port = process.env.PORT || 3000;
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.[hash].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true,
-            },
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.[hash].js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpg|svg|gif)$/,
+          loader: 'url-loader',
+          options: {
+            name: '[name].[ext]?[hash]',
+            limit: 5000,
           },
-        ],
-      },
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                minimize: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'app.css',
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-    }),
-  ],
-  devServer: {
-    host: 'localhost',
-    port: port,
-    open: true,
-    hot: true,
-  },
+    devServer: {
+      host: 'localhost',
+      port: port,
+      open: true,
+      hot: true,
+    },
+  };
 };
